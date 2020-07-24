8bf9d8299b76c411946d1dfca52f17fdfaa4fae5
MoeTerani <moe.terani@gmail.com>
2020-07-24 11:00:09 +0200

    sign up success, both FE & BE without Redux

--- client/src/components/auth/Register.tsx
+++ client/src/components/auth/Register.tsx
@@ -1,9 +1,115 @@
-import React from 'react';
+import React, { Fragment, useState } from 'react';
+import axios from 'axios';
+import { config } from 'process';
 
 interface Props {}
 
 const Register = (props: Props) => {
-  return <div>Register</div>;
+  const [formData, setFormData] = useState({
+    name: '',
+    email: '',
+    password: '',
+    password2: '',
+  });
+
+  const { name, email, password, password2 } = formData;
+
+  const updateValue = (e: any) =>
+    setFormData({ ...formData, [e.target.name]: e.target.value });
+
+  const handleSubmit = async (e: any) => {
+    e.preventDefault();
+    if (password !== password2) {
+      // setAlert('Passwords do not match', 'danger');
+      console.log('Passwords do not match');
+    } else {
+      const newUser = {
+        name,
+        email,
+        password,
+      };
+      // register({ name, email, password });
+      console.log(formData);
+      try {
+        const config = {
+          headers: { 'Content-Type': 'application/json' },
+        };
+        const body = JSON.stringify(newUser);
+
+        const res = await axios.post('/api/users', body, config);
+
+        console.log(res.data);
+      } catch (error) {
+        console.log(error);
+      }
+    }
+  };
+
+  return (
+    <Fragment>
+      <h1 className='large text-primary'>Sign Up</h1>
+      <p className='lead'>
+        <i className='fas fa-user'></i> Create Your Account
+      </p>
+      <form
+        className='form'
+        action='create-profile.html'
+        onSubmit={handleSubmit}
+      >
+        <div className='form-group'>
+          <input
+            type='text'
+            placeholder='Name'
+            name='name'
+            required
+            value={name}
+            onChange={updateValue}
+          />
+        </div>
+        <div className='form-group'>
+          <input
+            type='email'
+            placeholder='Email Address'
+            name='email'
+            value={email}
+            onChange={updateValue}
+          />
+          <small className='form-text'>
+            This site uses Gravatar so if you want a profile image, use a
+            Gravatar email
+          </small>
+        </div>
+        <div className='form-group'>
+          <input
+            type='password'
+            placeholder='Password'
+            name='password'
+            //@ts-ignore-start
+            minLength='6'
+            //@ts-ignore-end
+            value={password}
+            onChange={updateValue}
+          />
+        </div>
+        <div className='form-group'>
+          <input
+            type='password'
+            placeholder='Confirm Password'
+            name='password2'
+            //@ts-ignore-start
+            minLength='6'
+            //@ts-ignore-end
+            value={password2}
+            onChange={updateValue}
+          />
+        </div>
+        <input type='submit' className='btn btn-primary' value='Register' />
+      </form>
+      <p className='my-1'>
+        Already have an account? <a href='login.html'>Sign In</a>
+      </p>
+    </Fragment>
+  );
 };
 
 export default Register;
