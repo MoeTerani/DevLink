b88638eafe0e29ca3421072c3de3a3ceb8f3cc78
MoeTerani <moe.terani@gmail.com>
2020-07-28 22:48:53 +0200

    create profile form component DONE

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -11,6 +11,7 @@ import setAuthToken from './utils/setAuthToken';
 import { loadUser } from '../src/state/actions/auth-action';
 import Dashboard from './components/dashboard/Dashboard';
 import PrivateRoute from './components/routing/PrivateRoute';
+import CreateProfile from './components/profileForms/CreateProfile';
 
 if (localStorage.token) {
   setAuthToken(localStorage.token);
@@ -38,6 +39,11 @@ const App = () => {
             <Route exact path='/register' component={Register} />
             <Route exact path='/login' component={Login} />
             <PrivateRoute exact path='/dashboard' component={Dashboard} />
+            <PrivateRoute
+              exact
+              path='/create-profile'
+              component={CreateProfile}
+            />
           </Switch>
         </section>
       </div>
