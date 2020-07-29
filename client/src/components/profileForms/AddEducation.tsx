b9a1a3f3e30dc5e54bd926fa762bf5d5dd5d12e2
MoeTerani <moe.terani@gmail.com>
2020-07-29 22:20:15 +0200

    add education done

--- client/src/components/profileForms/AddEducation.tsx
+++ client/src/components/profileForms/AddEducation.tsx
@@ -1,9 +1,133 @@
-import React from 'react';
+import React, { Fragment, useState } from 'react';
+import { Link, useHistory } from 'react-router-dom';
+import { useDispatch } from 'react-redux';
+import { addEducationAction } from '../../state/actions/profile-action';
 
 interface Props {}
 
 const AddEducation = (props: Props) => {
-  return <div></div>;
+  const history = useHistory();
+  const dispatch = useDispatch();
+
+  const [formData, setFormData] = useState({
+    school: '',
+    degree: '',
+    fieldofstudy: '',
+    from: '',
+    to: '',
+    current: false,
+    description: '',
+  });
+
+  const [toDateDisabled, toggleDisabled] = useState(false);
+
+  const {
+    school,
+    degree,
+    fieldofstudy,
+    from,
+    to,
+    current,
+    description,
+  } = formData;
+
+  const updateValue = (e: any) =>
+    setFormData({ ...formData, [e.target.name]: e.target.value });
+
+  const handleSubmit = (e: any) => {
+    e.preventDefault();
+    console.log(formData);
+    dispatch(addEducationAction(formData, history));
+  };
+
+  return (
+    <Fragment>
+      <h1 className='large text-primary'>Add Your Education</h1>
+      <p className='lead'>
+        <i className='fas fa-code-branch' /> Add any school or bootcamp that you
+        have attended
+      </p>
+      <small>* = required field</small>
+      <form className='form' onSubmit={handleSubmit}>
+        <div className='form-group'>
+          <input
+            type='text'
+            placeholder='* School or Bootcamp'
+            name='school'
+            value={school}
+            onChange={updateValue}
+            required
+          />
+        </div>
+        <div className='form-group'>
+          <input
+            type='text'
+            placeholder='* Degree or Certificate'
+            name='degree'
+            value={degree}
+            onChange={updateValue}
+            required
+          />
+        </div>
+        <div className='form-group'>
+          <input
+            type='text'
+            placeholder='Field of Study'
+            name='fieldofstudy'
+            value={fieldofstudy}
+            onChange={updateValue}
+          />
+        </div>
+        <div className='form-group'>
+          <h4>From Date</h4>
+          <input type='date' name='from' value={from} onChange={updateValue} />
+        </div>
+        <div className='form-group'>
+          <p>
+            <input
+              type='checkbox'
+              name='current'
+              checked={current}
+              //@ts-ignore
+              value={current}
+              onChange={() => {
+                setFormData({ ...formData, current: !current });
+                toggleDisabled(!toDateDisabled);
+              }}
+            />{' '}
+            Current School
+          </p>
+        </div>
+        <div className='form-group'>
+          <h4>To Date</h4>
+          <input
+            type='date'
+            name='to'
+            value={to}
+            onChange={updateValue}
+            //@ts-ignore
+            disabled={toDateDisabled ? 'disabled' : ''}
+          />
+        </div>
+        <div className='form-group'>
+          <textarea
+            name='description'
+            //@ts-ignore
+            cols='30'
+            //@ts-ignore
+            rows='5'
+            placeholder='Program Description'
+            value={description}
+            onChange={updateValue}
+          />
+        </div>
+        <input type='submit' className='btn btn-primary my-1' />
+        <Link className='btn btn-light my-1' to='/dashboard'>
+          Go Back
+        </Link>
+      </form>
+    </Fragment>
+  );
 };
 
 export default AddEducation;
