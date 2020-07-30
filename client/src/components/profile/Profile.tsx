c5f613bd5c314add46186f5c8811d27539dd01a0
MoeTerani <moe.terani@gmail.com>
2020-07-30 20:32:04 +0200

    profile experience section done

--- client/src/components/profile/Profile.tsx
+++ client/src/components/profile/Profile.tsx
@@ -5,6 +5,7 @@ import { useParams, Link } from 'react-router-dom';
 import Spinner from '../layout/Spinner';
 import ProfileTop from './ProfileTop';
 import ProfileAbout from './ProfileAbout';
+import ProfileExperience from './ProfileExperience';
 
 interface Props {}
 
@@ -43,11 +44,11 @@ const Profile = (props: Props) => {
           <div className='profile-grid my-1'>
             <ProfileTop profile={profile} />
             <ProfileAbout profile={profile} />
-            {/* <div className='profile-exp bg-white p-2'>
+            <div className='profile-exp bg-white p-2'>
               <h2 className='text-primary'>Experience</h2>
               {profile.experience.length > 0 ? (
                 <Fragment>
-                  {profile.experience.map((experience) => (
+                  {profile.experience.map((experience: any) => (
                     <ProfileExperience
                       key={experience._id}
                       experience={experience}
@@ -58,7 +59,7 @@ const Profile = (props: Props) => {
                 <h4>No experience credentials</h4>
               )}
             </div>
-
+            {/*
             <div className='profile-edu bg-white p-2'>
               <h2 className='text-primary'>Education</h2>
               {profile.education.length > 0 ? (
---
+++ client/src/components/profile/ProfileExperience.tsx
@@ -0,0 +1,29 @@
+import React from 'react';
+import Moment from 'react-moment';
+import moment from 'moment';
+
+interface Props {}
+
+const ProfileExperience = ({ experience }: any) => {
+  const { company, title, location, to, from, description } = experience;
+  return (
+    <div>
+      <h3 className='text-dark'>{company}</h3>
+      <p>
+        <Moment format='YYYY/MM/DD'>{moment.utc(from)}</Moment> -{' '}
+        {!to ? ' Now' : <Moment format='YYYY/MM/DD'>{moment.utc(to)}</Moment>}
+      </p>
+      <p>
+        <strong>Position: </strong> {title}
+      </p>
+      <p>
+        <strong>Location: </strong> {location}
+      </p>
+      <p>
+        <strong>Description: </strong> {description}
+      </p>
+    </div>
+  );
+};
+
+export default ProfileExperience;
